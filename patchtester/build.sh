5717f46c92795e8e54e3f2bf8c37ee2539337b28
Michael Babker <mbabker@flbab.com>
2013-07-11 22:02:17 -0500

    Reset version number, build tweaks

--- patchtester/build.sh
+++ patchtester/build.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
-rm -rf site
-rm -rf admin
-rm -rf ../patchtester.tar.bz2
+rm -rf site admin patchtester.xml
+rm -rf ../com_patchtester.tar.bz2 ../com_patchtester.tar.gz ../com_patchtester.zip
 cp -r ../administrator/components/com_patchtester admin
 cp -r ../components/com_patchtester site
 rm -rf admin/backups/*.txt
 mv admin/patchtester.xml patchtester.xml
 tar jcf ../com_patchtester.tar.bz2 site admin patchtester.xml
 tar zcf ../com_patchtester.tar.gz site admin patchtester.xml
-
-rm -rf github
-mkdir github
-cp ../libraries/joomla/client/github.php github
-cp ../libraries/joomla/client/githubobject.php github
-cp ../libraries/joomla/client/curl.php github
-cp -r ../libraries/joomla/client/github github
-cp github.xml github
-tar jcf ../file_github.tar.bz2 github/*
-tar zcf ../file_github.tar.gz github/*
-
-tar jcf ../pkg_patchtester.tar.bz2 pkg_patchtester.xml ../com_patchtester.tar.bz2 ../file_github.tar.bz2
-tar zcf ../pkg_patchtester.tar.gz pkg_patchtester_gz.xml ../com_patchtester.tar.gz ../file_github.tar.gz
+zip -r ../com_patchtester.zip site/ admin/ patchtester.xml
