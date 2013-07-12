961a8c7a35a199b3f5cef60b098968567d75234a
Michael Babker <mbabker@flbab.com>
2013-07-11 21:03:26 -0500

    Use JHttp

--- administrator/components/com_patchtester/models/pull.php
+++ administrator/components/com_patchtester/models/pull.php
@@ -15,6 +15,31 @@ defined('_JEXEC') or die;
 class PatchtesterModelPull extends JModelLegacy
 {
 	/**
+	 * @var  JHttp
+	 */
+	protected $transport;
+
+	/**
+	 * Constructor
+	 *
+	 * @param   array  $config  An array of configuration options (name, state, dbo, table_path, ignore_request).
+	 *
+	 * @since   12.2
+	 * @throws  Exception
+	 */
+	public function __construct($config = array())
+	{
+		parent::__construct($config);
+
+		// Set up the JHttp object
+		$options = new JRegistry;
+		$options->set('userAgent', 'JPatchTester/1.0');
+		$options->set('timeout', 120);
+
+		$this->transport = JHttpFactory::getHttp($options, 'curl');
+	}
+
+	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
@@ -104,8 +129,7 @@ class PatchtesterModelPull extends JModelLegacy
 			throw new Exception(JText::_('COM_PATCHTESTER_REPO_IS_GONE'));
 		}
 
-		$patch = PTCurl::getAdapter($pull->diff_url)
-			->fetch()->body;
+		$patch = $this->transport->get($pull->diff_url)->body;
 
 		$files = $this->parsePatch($patch);
 
@@ -132,8 +156,7 @@ class PatchtesterModelPull extends JModelLegacy
 				$url = 'https://raw.github.com/' . $pull->head->user->login . '/' . $pull->head->repo->name . '/' .
 					$pull->head->ref . '/' . $file->new;
 
-				$file->body = PTCurl::getAdapter($url)
-					->fetch()->body;
+				$file->body = $this->transport->get($url)->body;
 			}
 		}
 
