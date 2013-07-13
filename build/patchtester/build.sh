0519aceb95517fadf24a49e51ef14ea47bd75a0e
Michael Babker <mbabker@flbab.com>
2013-07-12 20:13:14 -0500

    Rewrite build script for new locations

--- build/patchtester/build.sh
+++ build/patchtester/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-rm -rf site admin patchtester.xml
-rm -rf ../com_patchtester.tar.bz2 ../com_patchtester.tar.gz ../com_patchtester.zip
-cp -r ../administrator/components/com_patchtester admin
-cp -r ../components/com_patchtester site
-rm -rf admin/backups/*.txt
-mv admin/patchtester.xml patchtester.xml
-tar jcf ../com_patchtester.tar.bz2 site admin patchtester.xml
-tar zcf ../com_patchtester.tar.gz site admin patchtester.xml
-zip -r ../com_patchtester.zip site/ admin/ patchtester.xml
+rm -rf ../packaging && mkdir ../packaging
+rm -rf ../packages && mkdir ../packages
+cp -r ../../administrator/components/com_patchtester ../packaging/admin
+cp -r ../../components/com_patchtester ../packaging/site
+rm -rf ../packaging/admin/backups/*.txt
+mv ../packaging/admin/patchtester.xml ../packaging/patchtester.xml
+tar jcf ../packages/com_patchtester.tar.bz2 ../packaging/site/ ../packaging/admin/ ../packaging/patchtester.xml
+tar zcf ../packages/com_patchtester.tar.gz ../packaging/site/ ../packaging/admin ../packaging/patchtester.xml
+zip -r ../packages/com_patchtester.zip ../packaging/ite/ ../packaging/admin/ ../packaging/patchtester.xml
