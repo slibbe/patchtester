4382c77479364905341e9ce8b1eaf60c11377a5c
Michael Babker <mbabker@flbab.com>
2013-07-12 21:30:15 -0500

    Don't install the site component

--- build/patchtester/build.sh
+++ build/patchtester/build.sh
@@ -2,9 +2,8 @@
 rm -rf ../packaging && mkdir ../packaging
 rm -rf ../packages && mkdir ../packages
 cp -r ../../administrator/components/com_patchtester ../packaging/admin
-cp -r ../../components/com_patchtester ../packaging/site
 rm -rf ../packaging/admin/backups/*.txt
 mv ../packaging/admin/patchtester.xml ../packaging/patchtester.xml
-tar jcf ../packages/com_patchtester.tar.bz2 ../packaging/site/ ../packaging/admin/ ../packaging/patchtester.xml
-tar zcf ../packages/com_patchtester.tar.gz ../packaging/site/ ../packaging/admin ../packaging/patchtester.xml
-zip -r ../packages/com_patchtester.zip ../packaging/ite/ ../packaging/admin/ ../packaging/patchtester.xml
+tar jcf ../packages/com_patchtester.tar.bz2 ../packaging/admin/ ../packaging/patchtester.xml
+tar zcf ../packages/com_patchtester.tar.gz ../packaging/admin ../packaging/patchtester.xml
+zip -r ../packages/com_patchtester.zip ../packaging/admin/ ../packaging/patchtester.xml
