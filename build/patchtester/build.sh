0d85908bce0c6fe5c833e5efdfad21a9781684fc
Michael Babker <mbabker@flbab.com>
2013-07-12 21:50:35 -0500

    Build script tweaks

--- build/patchtester/build.sh
+++ build/patchtester/build.sh
@@ -4,6 +4,7 @@ rm -rf ../packages && mkdir ../packages
 cp -r ../../administrator/components/com_patchtester ../packaging/admin
 rm -rf ../packaging/admin/backups/*.txt
 mv ../packaging/admin/patchtester.xml ../packaging/patchtester.xml
-tar jcf ../packages/com_patchtester.tar.bz2 ../packaging/admin/ ../packaging/patchtester.xml
-tar zcf ../packages/com_patchtester.tar.gz ../packaging/admin ../packaging/patchtester.xml
-zip -r ../packages/com_patchtester.zip ../packaging/admin/ ../packaging/patchtester.xml
+cd ../packaging
+tar jcf ../packages/com_patchtester.tar.bz2 admin/ patchtester.xml
+tar zcf ../packages/com_patchtester.tar.gz admin patchtester.xml
+zip -r ../packages/com_patchtester.zip admin/ patchtester.xml
