92989cd67512e66b092c013ad5923bd7eb838174
Michael Babker <mbabker@flbab.com>
2013-09-28 20:43:51 -0500

    Miscellaneous tweaks
    - Updated README
    - Layouts for Hathor
    - Admin housecleaning on the repo

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ Patch Tester
 
 Easily apply changes from pull requests.
 
+To install the latest released version:
+1. Click on the releases link just above the file listing
+2. Click on the button to download either the .tar.bz2, .tar.gz, or .zip packages
+3. Log into your site's administrator section, go to Extension Manager > Install, and install the extension
+
+To use the latest code from this GitHub repo:
 1. Download files into Joomla install.
-2. Perform discover install.
-3. Go to Component Options and set organization name and repository name
+2. Log into your site's administrator section, go to Extension Manager > Discover, click the "Discover" button, and install the extension.
 
 Click Apply Patch to apply the proposed changes from the pull request.
 Click Revert Patch to revert an applied patch.
--- build/patchtester/build.sh
+++ build/patchtester/build.sh
@@ -2,9 +2,11 @@
 rm -rf ../packaging && mkdir ../packaging
 rm -rf ../packages && mkdir ../packages
 cp -r ../../administrator/components/com_patchtester ../packaging/admin
+cp -r ../../administrator/templates/hathor/html/com_patchtester ../packaging/hathor
 rm -rf ../packaging/admin/backups/*.txt
 mv ../packaging/admin/patchtester.xml ../packaging/patchtester.xml
+mv ../packaging/admin/script.php ../packaging/script.php
 cd ../packaging
-tar jcf ../packages/com_patchtester.tar.bz2 admin/ patchtester.xml
-tar zcf ../packages/com_patchtester.tar.gz admin patchtester.xml
-zip -r ../packages/com_patchtester.zip admin/ patchtester.xml
+tar jcf ../packages/com_patchtester.tar.bz2 admin/ hathor/ patchtester.xml script.php
+tar zcf ../packages/com_patchtester.tar.gz admin/ hathor/ patchtester.xml script.php
+zip -r ../packages/com_patchtester.zip admin/ hathor/ patchtester.xml script.php
