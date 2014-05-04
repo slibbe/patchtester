fdebf8b6b2ad46189ce2b0ff180c466e11dee4a7
Michael Babker <mbabker@flbab.com>
2014-05-04 16:55:06 -0500

    Update schema for non-MySQL databases

--- administrator/components/com_patchtester/install/sql/sqlsrv/install.sql
+++ administrator/components/com_patchtester/install/sql/sqlsrv/install.sql
@@ -1,14 +1,27 @@
+CREATE TABLE IF NOT EXISTS [#__patchtester_pulls] (
+  [id] [bigint] IDENTITY(1,1) NOT NULL,
+  [pull_id] [bigint] NOT NULL,
+  [title] [nvarchar](100) NOT NULL,
+  [description] [nvarchar](5000) NOT NULL DEFAULT '',
+  [pull_url] [nvarchar](255) NOT NULL,
+  [joomlacode_id] [nvarchar](255) NOT NULL DEFAULT '',
+ CONSTRAINT [PK_#__patchtester_pulls] PRIMARY KEY CLUSTERED
+(
+  [id] ASC
+) WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF)
+);
+
 CREATE TABLE [#__patchtester_tests](
-	[id] [bigint] IDENTITY(1,1) NOT NULL,
-	[pull_id] [bigint] NOT NULL,
-	[data] [nvarchar](MAX) NULL,
-	[patched_by] [bigint] NOT NULL,
-	[applied] [bigint] NOT NULL,
-	[applied_version] [nvarchar](25) NOT NULL,
-	[rating] [bigint] NOT NULL,
-	[comments] [nvarchar](255) NOT NULL DEFAULT '',
+  [id] [bigint] IDENTITY(1,1) NOT NULL,
+  [pull_id] [bigint] NOT NULL,
+  [data] [nvarchar](MAX) NULL,
+  [patched_by] [bigint] NOT NULL,
+  [applied] [bigint] NOT NULL,
+  [applied_version] [nvarchar](25) NOT NULL,
+  [rating] [bigint] NOT NULL,
+  [comments] [nvarchar](255) NOT NULL DEFAULT '',
  CONSTRAINT [PK_#__patchtester_tests] PRIMARY KEY CLUSTERED
 (
-	[id] ASC
+  [id] ASC
 ) WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF)
 );
--- administrator/components/com_patchtester/install/sql/sqlsrv/uninstall.sql
+++ administrator/components/com_patchtester/install/sql/sqlsrv/uninstall.sql
@@ -1,1 +1,2 @@
+DROP TABLE [#__patchtester_pulls];
 DROP TABLE [#__patchtester_tests];
